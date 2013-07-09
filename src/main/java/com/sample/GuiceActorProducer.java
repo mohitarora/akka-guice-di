e256d09ed72495bdff5d16978a0315d776f4443d
Mohit Arora <mohit.arora@lab49.com>
2013-07-09 01:25:19 -0400

    Optimized Imports

--- src/main/java/com/sample/GuiceActorProducer.java
+++ src/main/java/com/sample/GuiceActorProducer.java
@@ -3,10 +3,6 @@ package com.sample;
 import akka.actor.Actor;
 import akka.actor.IndirectActorProducer;
 import com.google.inject.Injector;
-import com.google.inject.Key;
-import com.google.inject.TypeLiteral;
-
-import java.lang.reflect.Type;
 
 /**
  * An actor producer that lets Google Guice create the Actor instances.
