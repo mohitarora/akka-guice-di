2f04c11d7273ff80be5ccb0957981da748ec1336
Mohit Arora <mohit.arora@lab49.com>
2013-07-09 01:19:27 -0400

    Added qualifier for actors.

--- src/main/java/com/sample/GuiceActorProducer.java
+++ src/main/java/com/sample/GuiceActorProducer.java
@@ -15,9 +15,9 @@ public class GuiceActorProducer implements IndirectActorProducer {
 
     private final Injector injector;
 
-    private final Type actorType;
+    private final Class<Actor> actorType;
 
-    public GuiceActorProducer(Injector injector, Type actorType) {
+    public GuiceActorProducer(Injector injector, Class<Actor> actorType) {
         this.injector = injector;
         this.actorType = actorType;
     }
@@ -25,11 +25,11 @@ public class GuiceActorProducer implements IndirectActorProducer {
 
     @Override
     public Actor produce() {
-        return (Actor) injector.getInstance(Key.get(TypeLiteral.get(actorType)));
+        return injector.getInstance(actorType);
     }
 
     @Override
     public Class<? extends Actor> actorClass() {
-        return (Class<Actor>) actorType;
+        return actorType;
     }
 }
